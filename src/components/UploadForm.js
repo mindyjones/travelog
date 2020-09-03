faa62215159030324da405485c1f9d45fb0e516b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-09-03 14:15:15 +0530

    Upload Form Component with State and Event handling

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const types = ["image/png", "image/jpg"];
+  const [error, setError] = useState(null);
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError("");
+    } else {
+      setFile(null);
+      setError("Please select an image file (png or jpeg)");
+    }
+  };
+
   return (
     <form>
-      <input onChange={} type="file" />
+      <input onChange={changeHandler} type="file" />
+      <div className="output">
+        {error && <div className="error"> {error} </div>}
+        {file && <div> {file.name} </div>}
+      </div>
     </form>
   );
 };
