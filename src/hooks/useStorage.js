d72ee7d427c9a2d9cf7679683977c7a73892aa31
Abhinav Dhiman <abhinavg916@gmail.com>
2020-09-03 14:32:31 +0530

    Hook setup for firebase and progress bar

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { projectStorage } from "../firebase/config";
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    // References
+    const storageRef = projectStorage.ref(file.name);
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    ); // put() is of asynchronous nature
+  }, [file]);
+
+  return {
+    progress,
+    url,
+    error,
+  };
+};
+
+export default useStorage;
